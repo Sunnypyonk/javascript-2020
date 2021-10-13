96b1a1cea01cc3c52bde9a870f2a6e2c42a7ee23
Sunnypyonk <66201882+Sunnypyonk@users.noreply.github.com>
2021-10-13 16:34:24 +0300

    Add comments to string

--- 03_string.js
+++ 03_string.js
@@ -1,33 +1,34 @@
-// const name = 'Владилен'
-// const age = 26
-//
-// function getAge() {
-//   return age
-// }
+const name = 'Владилен'
+const age = 26
 
-// const output = 'Привет, меня зовут ' + name + ' и мой возраст ' + age + ' лет.'
-// const output = `Привет, меня зовут ${name} и мой возраст ${age < 30 ? 'A' : 'B'} лет.`
-// const output = `
-//   <div>This is div</div>
-//   <p>this is o</p>
-// `
+function getAge() {
+  return age
+}
 
-// console.log(output)
+const output = 'Привет, меня зовут ' + name + ' и мой возраст ' + age + ' лет.'
+const output = `Привет, меня зовут ${name} и мой возраст ${age < 30 ? 'A' : 'B'} лет.` // обратные ковычки(похоже на f-string)
+//Можно многострочно, переносы строк учитываются
+const output = `
+  <div>This is div</div>
+  <p>this is o</p>
+`
 
-// const name = 'Владилен'
-// console.log(name.length)
-// console.log(name.toUpperCase())
-// console.log(name.toLowerCase())
-// console.log(name.charAt(2))
-// console.log(name.indexOf('!'))
-// console.log(name.toLowerCase().startsWith('влад'))
-// console.log(name.startsWith('Влад'))
-// console.log(name.endsWith('ен!'))
-// console.log(name.repeat(3))
-// const string = '     password      '
-// console.log(string.trim())
-// console.log(string.trimLeft())
-// console.log(string.trimRight())
+console.log(output)
+
+const name = 'Владилен'
+console.log(name.length)
+console.log(name.toUpperCase()) //переводит в заглавные
+console.log(name.toLowerCase()) //переводит в строчные
+console.log(name.charAt(2)) //выведет какой символ на 2м индексе
+console.log(name.indexOf('!')) //проверяет,есть ли в строке. Выводит индекс начала. Если нету -1.
+console.log(name.toLowerCase().startsWith('влад'))
+console.log(name.startsWith('Влад')) //проверка начинается ли строка с ...
+console.log(name.endsWith('ен!'))  //проверка заканчивается ли строка  ...
+console.log(name.repeat(3)) //повторяет строку такое кол-во раз
+const string = '     password      '
+console.log(string.trim()) //очищает пробелы
+console.log(string.trimLeft())//очищает пробелы слева
+console.log(string.trimRight())//очищает пробелы справа
 
 function logPerson(s, name, age) {
   if (age < 0) {
